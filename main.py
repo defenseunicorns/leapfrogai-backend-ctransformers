dde396eff8b5bec45d7d5b710272c8c75c8395ed
Brandi McCall <117415095+bm54cloud@users.noreply.github.com>
2023-11-05 12:05:20 -0700

    Added chat completion implementation (#26)

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from typing import Any, Generator
 
 from ctransformers import AutoModelForCausalLM
 
+from google.protobuf.internal.containers import RepeatedCompositeFieldContainer
+
+# completion
 from leapfrogai import (
     CompletionChoice,
     CompletionFinishReason,
@@ -14,7 +17,44 @@ from leapfrogai import (
     serve,
 )
 
+# chat
+from leapfrogai import (
+    ChatCompletionRequest,
+    ChatCompletionChoice,
+    ChatItem,
+    ChatRole,
+    ChatCompletionResponse
+)
+
 logger = logging.getLogger(__name__)
+
+# Prompt Templates
+SYSTEM_FORMAT = "<|im_start|>system\n{}<|im_end|>\n"
+USER_FORMAT = "<|im_start|>user\n{}<|im_end|>\n"
+ASSISTANT_FORMAT = "<|im_start|>assistant\n{}<|im_end|>\n"
+# what gets appended to the end of the prompt to open the assistant's part of the conversation
+RESPONSE_PREFIX = ASSISTANT_FORMAT.split("{}")[0]
+
+def chat_items_to_prompt(chat_items: RepeatedCompositeFieldContainer[ChatItem]) -> str:
+    """Converts a repeated ChatItem from a ChatCompletionRequest proto into a string
+
+    This is the actual string that gets fed into the model to generate the outputs
+    """
+    prompt = ""
+    for item in chat_items:
+        if item.role == ChatRole.SYSTEM:
+            prompt += SYSTEM_FORMAT.format(item.content)
+        elif item.role == ChatRole.ASSISTANT:
+            prompt += ASSISTANT_FORMAT.format(item.content)
+        elif item.role == ChatRole.USER:
+            prompt += USER_FORMAT.format(item.content)
+        elif item.role == ChatRole.FUNCTION:
+            logging.warning(
+                "ChatRole FUNCTION is not implemented for this model and this ChatItem will be ignored."
+            )
+    # add the response prefix to start the model's reponse
+    prompt += RESPONSE_PREFIX
+    return prompt
 
 class CTransformers:
     MODEL_PATH = ".model/synthia-7b-v2.0.Q4_K_M.gguf"
@@ -27,7 +67,60 @@ class CTransformers:
             model_type=self.MODEL_TYPE,
             context_length=8192,
         )
-        logger.info("Loaded model.") 
+        logger.info("Loaded model.")
+
+    def chat_stream(self, request: ChatCompletionRequest) -> Generator[str, Any, Any]:
+        """Implements the logic for the chat endpoint"""
+        # convert chat items to prompt and tokenize
+        prompt = chat_items_to_prompt(request.chat_items)
+
+        # create text streamer and validate parameters
+        max_new_tokens = 1536 if request.max_new_tokens == 0 else request.max_new_tokens
+        temperature = 0.1 if request.temperature == 0.0 else request.temperature
+        top_p = 1.0 if request.top_p == 0.0 else request.top_p
+        top_k = 0 if request.top_k == 0.0 else int(request.top_k)
+
+        for text in self.llm(
+            prompt=prompt,
+            max_new_tokens=max_new_tokens,
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k,
+            stop=["</s>","<|im_end|>"],
+            stream=True
+        ):
+            print(text)
+            yield text
+
+    def ChatComplete(
+            self, request: ChatCompletionRequest, context: GrpcContext
+    ) -> ChatCompletionResponse:
+        logger.info("CHATCOMPLETE:\n---")
+        chat_stream = self.chat_stream(request)
+
+        content = ""
+        for text_chunk in chat_stream:
+            content += text_chunk
+
+        item = ChatItem(role=ChatRole.ASSISTANT, content=content)
+        choice = ChatCompletionChoice(index=0, chat_item=item)
+        logger.info(choice)
+        logger.info("CHATCOMPLETEEND:\n---")
+        return ChatCompletionResponse(choices=[choice])
+    
+    def ChatCompleteStream(
+        self, request: ChatCompletionRequest, context: GrpcContext
+    ) -> Generator[ChatCompletionResponse, Any, Any]:
+        logger.info("CHATCOMPLETESTREAM:\n---")
+        chat_stream = self.chat_stream(request)
+
+        for text_chunk in chat_stream:
+            item = ChatItem(role=ChatRole.ASSISTANT, content=text_chunk)
+            choice = ChatCompletionChoice(index=0, chat_item=item)
+
+            yield ChatCompletionResponse(choices=[choice])
+
+        logger.info("CHATCOMPLETESTREAMEND:\n---")
 
     def Complete(
         self, request: CompletionRequest, context: GrpcContext
